2abfba8bdf6df25a0a58a6a3f3ad09433fa74263
Richard <Richard@w.com>
2025-10-11 14:56:06 +0800

    u a

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const ethers = require('ethers')
 
 // const url = 'https://gwan-ssl.wandevs.org:56891'
-const url = 'http://127.0.0.1:8545'
-const begin = 4046000
+const url = 'http://127.0.0.1:26891'
+const begin = 406000
 // const MainnetPow2PosUpgradeBlockNumber = 4046000
 const account = "0x9da26fc2e1d6ad9fdd46138906b0104ae68a65d8"
 
 async function main() {
-  let httpProvider = new ethers.providers.JsonRpcProvider('https://gwan-ssl.wandevs.org:56891')
+  let httpProvider = new ethers.providers.JsonRpcProvider(url)
 
   let cur = await httpProvider.getBlockNumber()
   console.log("cur:", cur)
@@ -17,18 +17,20 @@ async function main() {
   let lastBlock = b
 
   for(let i=cur; i>begin; i--) {
-    let b = await httpProvider.getBlock(cur)
+    let b = await httpProvider.getBlock(i)
+    //console.log("i:", b.number)
     let epochID = parseInt(b.timestamp/86400);
-    if(epochID != lastEpoch) {
-      try {
-        await httpProvider.getBalance(account, i)
-      }catch(err) {
-        console.log("err:", err)
-      }
+    if(true || epochID != lastEpoch) {
+	try{
+        	await httpProvider.getBalance(account, i)
+        	console.log("Epoch OK", lastEpoch, i)
+	}catch(err){
+		console.log("NOT OK", lastEpoch, i)
+	}
+
       lastBlock = i
       lastEpoch = epochID
-      console.log("lastEpoch:", lastEpoch)
-      i -= 15000
+      i -= 10
     }
   }
 }
